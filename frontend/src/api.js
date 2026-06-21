13189ebaf581b2e39fbcd5d9ce5cc9a1af533fa9
Moiz Rehan <moizrehan6ji@gmail.com>
2026-06-21 20:05:31 +0500

    fix vercel api proxy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,5 @@
-const API_BASE = (import.meta.env.VITE_API_URL || '').replace(/\/$/, '');
-
 export async function api(path, options = {}) {
-  const url = `${API_BASE}${path}`;
-
-  const res = await fetch(url, {
+  const res = await fetch(path, {
     credentials: 'include',
     headers: { 'Content-Type': 'application/json', ...(options.headers || {}) },
     ...options,
